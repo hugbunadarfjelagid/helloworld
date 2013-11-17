fe7e2df93553343f90118876bce03ff40b1b5de6
Skúli Arnlaugsson <skuli.arnlaugsson@gmail.com>
2013-11-17 23:41:19 +0000

    Added a simple 'get' url route /hello
    For testing setup on different environments. This should work everywhere, it's
    the same as the demo code from Spark Framework.

--- src/main/java/is/ru/helloworld/HelloWorld.java
+++ src/main/java/is/ru/helloworld/HelloWorld.java
@@ -9,6 +9,13 @@ public class HelloWorld {
         
         setPort(Integer.valueOf(System.getenv("PORT")));
 
+        get(new Route("/hello") {
+            @Override
+            public Object handle(Request request, Response response) {
+                return "Hello World!";
+            }
+        });
+        
         post(new Route("/add") {
             @Override
             public Object handle(Request request, Response response) {
